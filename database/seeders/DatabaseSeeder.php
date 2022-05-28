67d1e6d1844f7368f66e8618a2a1f7542c070a8c
unknown <bouzerb90@gmail.com>
2022-05-28 23:47:31 +0100

    livewire

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        
+        $this->call(BloodTableSeeder::class);
+        $this->call(NationalitieTableSeeder::class);
+        $this->call(ReligionTableSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
